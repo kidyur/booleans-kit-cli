68cc42264a88725bf1fb50a2de3e11c60f79a2d0
Kirill Pindyurin <kipindyurin@gmail.com>
2026-02-28 21:18:35 +0300

    new: def entry() instead of entry_dnf() _cnf() _simplify()

--- logic_kit.py
+++ logic_kit.py
@@ -1,11 +1,8 @@
-from sympy import logic
-import re
+from sympy import to_dnf, simplify_logic, to_cnf
+from sys import exit
+from re import search 
 
 alphabet = "abcdefghijklmnopqrstuvwxyz"
-
-def simplify(expr):
-  expr = format(expr)
-  return logic.simplify_logic(expr)
 
 
 def remove_spaces(expr):
@@ -18,7 +15,7 @@ def remove_dublicate_negs(expr):
 
 
 def clarify_conjunction(expr):
-  seq_found = re.search(r"(?:(?:~{0,}[A-z])|~{0,}\(.*\)){2}", expr)
+  seq_found = search(r"(?:(?:~{0,}[A-z])|~{0,}\(.*\)){2}", expr)
   while seq_found:
     pat = seq_found.group(0)
     bracketCnt = 0
@@ -46,17 +43,17 @@ def clarify_conjunction(expr):
       else:
         b += l
     expr = expr.replace(pat, f"{a} & {b}", 1)
-    seq_found = re.search(r"(?:(?:~{0,}[A-z])|~{0,}\([^\(\)]*\)){2}", expr)
+    seq_found = search(r"(?:(?:~{0,}[A-z])|~{0,}\([^\(\)]*\)){2}", expr)
   return expr    
 
 
 def replace_operator(operator, rep, expr):
-  op_found = re.search(r"[^&|+>=!/]+\{}[^&|+>=!/]+".format(operator), expr)
+  op_found = search(r"[^&|+>=!/]+\{}[^&|+>=!/]+".format(operator), expr)
   while op_found:
     pat = op_found.group(0)
     operands = pat.split(operator)
     expr = expr.replace(pat, f"{rep}({operands[0]}, {operands[1]})", 1)
-    op_found = re.search(r"[^&|+>=!/]+\{}[^&|+>=!/]+".format(operator), expr)
+    op_found = search(r"[^&|+>=!/]+\{}[^&|+>=!/]+".format(operator), expr)
   return expr
 
 
@@ -64,39 +61,68 @@ def format(expr):
   expr = remove_spaces(expr)
   expr = remove_dublicate_negs(expr)
   expr = clarify_conjunction(expr)
-
   # Important to save the operators order to keep it correct
   expr = replace_operator('=', "Equivalent", expr)
   expr = replace_operator('>', "Implies", expr)
   expr = replace_operator('!', "Nor", expr)
   expr = replace_operator('/', "Nand", expr)
   expr = replace_operator('+', "Xor", expr)
-
   return expr
+
+
+def show_menu():
+  print("""
+    [menu] Choose the option:
+      "1" to simplify your expression 
+      "2" to convert show your expression to dnf form
+      "3" to convert it to cnf form
+      "exit" to exit the program
+      "back" to return to main menu
+      "help" to get hints
+  """)
+  choice = input(">>> ")
+  if (choice == "exit"): 
+    exit()
+  if (choice == "help"):
+    start()
+  if (choice == "1"):
+    entry("[simplify]", simplify_logic)
+  if (choice == "2"):
+    entry("[dnf]", to_dnf)
+  if (choice == "3"):
+    entry("[cnf]", to_cnf)
+  else:
+    show_menu()
+
+
+def entry(program, callback):
+  print(f"{program} Type your expression:")
+  expr = ""
+  while (expr != "back"):
+    expr = input(">>> ")
+    if (expr == "exit"):
+      exit()
+    if (expr == "back"):
+      show_menu()
+    print(callback(format(expr)))
+    
 
 def start():
   print(f"""
     +--------+------------+--------+------------+
     | Symbol | Operation  | Symbol | Operation  |   Note that:
-    +--------+------------+--------+------------+   ab = a & b
-    |   &    |    AND     |   !    |    NOR     |   (...)(...) = (...) & (...)
+    +--------+------------+--------+------------+   1) ab = a & b
+    |   &    |    AND     |   !    |    NOR     |   2) (...)(...) = (...) & (...)
     +--------+------------+--------+------------+  
-    |   |    |    OR      |   /    |    NAND    |
+    |   |    |    OR      |   /    |    NAND    |    
     +--------+------------+--------+------------+  
     |   +    |    XOR     |   >    |  IMPLIES   |
-    +--------+------------+--------+------------+  
-    |   =    | EQUIVALENT |   ~    |    NOT     |
+    +--------+------------+--------+------------+   
+    |   =    | EQUIVALENT |   ~    |    NOT     |   
     +--------+------------+--------+------------+                      
   """)
-  
-  expr = ""
-  while (expr != "exit"):
-    print("Input your logic expression:")
-    expr = input(">>> ")
-    if (expr == "exit"): break
-    print(f"""
-      {simplify(expr)}
-    """)
+  show_menu()  
+
 
 if __name__ == "__main__":
   start()
